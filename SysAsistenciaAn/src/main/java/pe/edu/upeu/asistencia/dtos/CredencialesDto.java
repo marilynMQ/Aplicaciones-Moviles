550b4c88b4dc15abd001b76b3595ab80f59ca154
marilynMQ <87881181+marilynMQ@users.noreply.github.com>
2023-11-05 19:42:34 -0500

    Delete SysAsistenciaAn directory

--- SysAsistenciaAn/src/main/java/pe/edu/upeu/asistencia/dtos/CredencialesDto.java
+++
@@ -1,3 +0,0 @@
-package pe.edu.upeu.asistencia.dtos;
-
-public record CredencialesDto (String correo, char[] password) { }
--- SysAsistenciaAn/src/main/java/pe/edu/upeu/asistencia/dtos/ErrorDto.java
+++
@@ -1,3 +0,0 @@
-package pe.edu.upeu.asistencia.dtos;
-
-public record ErrorDto (String message) { }
